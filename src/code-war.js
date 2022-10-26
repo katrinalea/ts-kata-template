0998b20918f953d23ffed42a4d19fdb9c1b6365d
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-10-26 17:04:44 +0100

    working code war

--- src/code-war.js
+++
@@ -1,7 +0,0 @@
-function containsValue(array, value) {
-    return array.includes(value)
-}
-
-console.log(containsValue([1,2,3,4], 4), "expected = true")
-console.log(containsValue(["hello", "goodbye"], "hell"))
-
